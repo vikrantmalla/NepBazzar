a871a0c25e9228f6a2799fb9faae271a1b360401
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-19 21:30:43 +0545

    feat: add a navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div className="mb-5 flex justify-between items-center px-8 py-4 bg-sky-600">
+    <h1 className="text-base font-medium text-white">NepBazzer</h1>
+  </div>
+</template>
