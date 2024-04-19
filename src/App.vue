74b8177ec9d7a6eea4f2745fa7b0e6ca5e761dd9
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-19 21:51:38 +0545

    feat: add a items list in card

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
   <header>
     <NavBar />
   </header>
+  <main>
+    <ItemList/>
+  </main>
 </template>
 
 <script setup lang="ts">
 import NavBar from "./components/NavBar.vue";
+import ItemList from "./components/ItemList.vue";
 </script>
