4b701aad997a50562505288d00ca8d63827769f2
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-20 15:19:01 +0545

    feat: add a skeleton card component

---
+++ src/components/shared/SkeletonCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="skeleton-item">
+    <div class="skeleton-image h-40 mb-4 rounded-md bg-slate-400"></div>
+    <div class="skeleton-heading h-6 mb-2 bg-slate-400"></div>
+    <div class="skeleton-price h-4 mb-2 bg-slate-400"></div>
+    <div class="skeleton-description h-4 mb-2 bg-slate-400"></div>
+    <div class="skeleton-category h-4 mb-2 bg-slate-400"></div>
+    <div class="skeleton-rating h-4 mb-2 bg-slate-400"></div>
+    <div class="skeleton-button h-10 w-20 bg-slate-400"></div>
+  </div>
+</template>
