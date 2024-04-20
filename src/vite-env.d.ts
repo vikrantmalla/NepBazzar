2dcb9ade31b7a19201beab0d5c5bcc02a5f4fb2a
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-20 13:23:22 +0545

    fix: module path problem in vuex package

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+/// https://stackoverflow.com/questions/41292559/could-not-find-a-declaration-file-for-module-module-name-path-to-module-name
+declare module "vuex";
