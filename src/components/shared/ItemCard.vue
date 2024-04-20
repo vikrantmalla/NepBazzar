5cbe4eab5b5eb16dcbdbeb0ab530681b352f69c8
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-20 13:24:54 +0545

    fix: UI issues and add a type saftey

--- src/components/shared/ItemCard.vue
+++ src/components/shared/ItemCard.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <img
-      :src="item.image"
-      :alt="item.title"
-      class="w-full h-auto mb-4 rounded-md"
-    />
-    <h3 class="text-lg font-semibold mb-2">{{ item.title }}</h3>
+    <img :src="item.image" :alt="item.title" class="h-40 mb-4 rounded-md" />
+    <h3 class="text-lg font-semibold mb-2">{{ shortHeading }}...</h3>
     <p class="text-gray-600 mb-2">${{ item.price }}</p>
-    <p class="text-gray-700 mb-2">{{ item.description }}</p>
+    <p class="text-gray-700 mb-2">{{ shortDescription }}...</p>
     <p class="text-gray-500 mb-2">{{ item.category }}</p>
     <div class="flex items-center mb-2">
       <span class="text-gray-600"
@@ -21,6 +17,13 @@
     </button>
   </div>
 </template>
-<script setup>
-const props = defineProps(['item'])
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { Product } from "../../types/data";
+const props = defineProps<{
+  item: Product;
+}>();
+const { item } = props;
+const shortHeading = item.title.slice(0, 50);
+const shortDescription = item.description.slice(0, 100);
 </script>
