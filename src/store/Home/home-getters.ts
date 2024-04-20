35fc57e5b59afaf73cca8b66f9d0416b7319da26
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-20 20:46:01 +0545

    feat: add a filter feature

--- src/store/Home/home-getters.ts
+++ src/store/Home/home-getters.ts
@@ -3,4 +3,18 @@ import { Product } from "../../types/data";
 export const getters = {
   isLoading: (state: { loading: boolean }) => state.loading,
   getProductData: (state: { productData: Product }) => state.productData,
+  uniqueCategories(state: {uniqueCategories: string[]}) {
+    return state.uniqueCategories;
+  },
+  filteredProducts: (state: {
+    selectedCategory: string;
+    productData: Product[];
+  }) => {
+    if (!state.selectedCategory) {
+      return state.productData; // Return all products if no category is selected
+    }
+    return state.productData.filter(
+      product => product.category === state.selectedCategory
+    );
+  },
 };
