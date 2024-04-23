a15e8fc57c00255076ebdec8a1acf39d59446add
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-23 22:25:36 +0545

    feat: add a slider popup to show a cart items

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,12 @@
   </header>
   <main class="w-[90%] m-auto">
     <ItemList />
-    <div v-if="showSlider" class="fixed inset-y-0 left-0 bg-slate-600 bg-opacity-50 w-full">
-      <div class="absolute inset-y-0 left-0 w-64 bg-gray-200 shadow-lg">
-        <div class="p-4">
-          Slider Content
-        </div>
-      </div>
-    </div>
+    <CartSlider />
   </main>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import NavBar from "./components/NavBar.vue";
 import ItemList from "./components/ItemList.vue";
-import { useStore } from "vuex";
-
-const store = useStore();
-const { getters } = store;
-
-const showSlider = computed(() => getters["home/showSlider"]);
+import CartSlider from "./components/shared/CartSlider.vue";
 </script>
