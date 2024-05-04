9e3136436ec14ab83ec5cb561813c8338bb6d9ba
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-04 21:41:25 +0545

    Revert "feat: add a fetch categories api endpoint"
    This reverts commit b5d7bd816c4554a7fc435c7ba4cec9c6c8617458.

--- src/service/endpoints/index.ts
+++ src/service/endpoints/index.ts
@@ -1,4 +1,3 @@
 const apiBaseURL = import.meta.env.VITE_VUE_APP_API_ENDPOINT
 
 export const FETCH_PRODUCTS = `${apiBaseURL}/products`
-export const FETCH_CATEGORIES = `${apiBaseURL}/products/categories`
