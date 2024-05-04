b5d7bd816c4554a7fc435c7ba4cec9c6c8617458
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-04 21:39:22 +0545

    feat: add a fetch categories api endpoint

--- src/service/endpoints/index.ts
+++ src/service/endpoints/index.ts
@@ -1,3 +1,4 @@
 const apiBaseURL = import.meta.env.VITE_VUE_APP_API_ENDPOINT
 
 export const FETCH_PRODUCTS = `${apiBaseURL}/products`
+export const FETCH_CATEGORIES = `${apiBaseURL}/products/categories`
