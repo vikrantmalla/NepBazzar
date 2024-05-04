44a675d671094f587042450cb5113205c1abc847
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-04 21:40:42 +0545

    refactor: remove a unmounted api call

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -7,16 +7,13 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, computed } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
 import Search from "./shared/Search.vue";
 import SideBar from "./SideBar.vue";
 import Item from "./Item.vue"
 
 const store = useStore();
-const { getters, dispatch } = store;
+const { getters } = store;
 const loading = computed(() => getters["home/isLoading"]);
-onMounted(async () => {
-  await dispatch("home/fetchProductData");
-});
 </script>
