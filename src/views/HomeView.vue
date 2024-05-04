2006a49f0a7175b90c702d94d6a7dd8696e46850
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-04 21:48:24 +0545

    refactor: add a onMounted function in global level

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,26 @@
 <template>
-      <ItemList />
-      <div v-if="showSlider" class="fixed inset-y-0 left-0 bg-slate-600 bg-opacity-50 w-full">
-        <div class="absolute inset-y-0 left-0 w-64 bg-gray-200 shadow-lg">
-          <div class="p-4">
-            Slider Content
-          </div>
-        </div>
-      </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { computed } from "vue";
-  import ItemList from "../components/ItemList.vue";
-  import { useStore } from "vuex";
-  
-  const store = useStore();
-  const { getters } = store;
-  
-  const showSlider = computed(() => getters["home/showSlider"]);
-  </script>
-  
+  <ItemList />
+  <div
+    v-if="showSlider"
+    class="fixed inset-y-0 left-0 bg-slate-600 bg-opacity-50 w-full"
+  >
+    <div class="absolute inset-y-0 left-0 w-64 bg-gray-200 shadow-lg">
+      <div class="p-4">Slider Content</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, computed } from "vue";
+import ItemList from "../components/ItemList.vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const { getters, dispatch } = store;
+
+const showSlider = computed(() => getters["home/showSlider"]);
+onMounted(async () => {
+  await dispatch("home/fetchProductData");
+  await dispatch("home/fetchCategoriesList");
+});
+</script>
