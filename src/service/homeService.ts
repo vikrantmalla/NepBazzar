cd17709ab89d176ae5fa31d81e5c102118ef248e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-10 22:18:04 +0545

    feat: add a fetch single product details services

--- src/service/homeService.ts
+++ src/service/homeService.ts
@@ -1,18 +1,24 @@
-import { FETCH_CATEGORIES, FETCH_PRODUCTS } from "./endpoints";
+import { FETCH_CATEGORIES, FETCH_PRODUCTS, FETCH_SINGLE_PRODUCTS } from "./endpoints";
 
 export function useProductFetch() {
   const fetchProduct = async () => {
-      const response = await fetch(FETCH_PRODUCTS);
-      return response; 
+    const response = await fetch(FETCH_PRODUCTS);
+    return response;
   };
 
-  const fetchProductCategories =  async () => {
-    const response = await fetch(FETCH_CATEGORIES)
+  const fetchSingleProduct = async (id: number) => {
+    const response = await fetch(`FETCH_SINGLE_PRODUCTS/${id}`);
     return response;
-  }
+  };
+
+  const fetchProductCategories = async () => {
+    const response = await fetch(FETCH_CATEGORIES);
+    return response;
+  };
 
   return {
     fetchProduct,
-    fetchProductCategories
+    fetchSingleProduct,
+    fetchProductCategories,
   };
 }
