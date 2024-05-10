9793e5b7a1daafbddd412756545ee16b1c7b86d1
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-10 22:19:41 +0545

    feat: add a singleProductData state and add a type safety

--- src/store/Home/home-state.ts
+++ src/store/Home/home-state.ts
@@ -3,6 +3,7 @@ import { StoreType } from "../../types/store";
 export const state: StoreType.HomeState = {
   loading: false,
   productData: [],
+  singleProductData: [],
   uniqueCategories: [],
   showSlider: false,
   selectedCategory: null,
--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -4,6 +4,7 @@ declare namespace StoreType {
   interface HomeState {
     loading: boolean;
     productData: Product[];
+    singleProductData: Product[];
     uniqueCategories: string[];
     showSlider: boolean;
     selectedCategory: null | string;
