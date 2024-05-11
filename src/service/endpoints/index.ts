b0bf3047ee8a04038f48edcdcaf0a5453e5ecf08
Vikrant Malla <vikrantmalla999@gmail.com>
2024-05-11 22:19:24 +0545

    fix: add a ID as a params

--- src/service/endpoints/index.ts
+++ src/service/endpoints/index.ts
@@ -1,5 +1,5 @@
 const apiBaseURL = import.meta.env.VITE_VUE_APP_API_ENDPOINT
 
 export const FETCH_PRODUCTS = `${apiBaseURL}/products`
-export const FETCH_SINGLE_PRODUCTS = `${apiBaseURL}/products`
+export const FETCH_SINGLE_PRODUCTS = (id: number) => `${apiBaseURL}/products/${id}`;
 export const FETCH_CATEGORIES = `${apiBaseURL}/products/categories`
